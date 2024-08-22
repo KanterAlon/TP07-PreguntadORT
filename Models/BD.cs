7d0aca433886444b45c2149763b7f471346f8f5b
ALON KANTER <95320323@ort>
2024-08-22 13:36:02 -0300

    Creacion MVC

---
+++ Models/BD.cs
@@ -0,0 +1,12 @@
+using System.Data.SqlClient;
+using Dapper;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BD
+{
+    private static string _connectionString = @"Server=localhost;Database=PreguntadOrt;Trusted_Connection=True;";
+
+    //public static vo...
+
+}
