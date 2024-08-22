bace78cff02a71044715ec1158f0b4bc8d4c2dcf
ALON KANTER <95320323@ort>
2024-08-22 15:04:46 -0300

    temp

--- obj/Debug/net8.0/TP07-PreguntadORT.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP07-PreguntadORT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP07-PreguntadORT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0aca433886444b45c2149763b7f471346f8f5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8051f3cbe01c33c480ef36d6b18ff4353619a9c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP07-PreguntadORT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP07-PreguntadORT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
