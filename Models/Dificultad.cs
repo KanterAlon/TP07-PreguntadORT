96f96ce61f38039bd69158e92388b396a4c3b3d3
ALON KANTER <95320323@ort>
2024-09-18 09:28:49 -0300

    configurar juego avancado

--- Models/Dificultad.cs
+++ Models/Dificultad.cs
@@ -1,6 +1,7 @@
 public class Dificultad
 {
     public int IdDificultad { get; set; }
-    public string?
-     Nombre { get; set; }
+    public string? Nombre { get; set; }
+    public string? Foto { get; set; }
+
 }
